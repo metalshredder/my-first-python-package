3e6e430cfa7c620e34ae274fa881e5c64430e493
metalshredder <106719069+metalshredder@users.noreply.github.com>
2022-06-01 21:25:19 -0600

    added docstring for subtraction_function()

--- main_function.py
+++ main_function.py
@@ -6,4 +6,23 @@ def addition_function(x,y):
 
 
 def subtraction_function(x,y):
+  """
+  This does subtraction.
+  
+  This takes two integers and returns the difference between
+  the first and the second integer.
+  
+  -----------------------------------------------------------
+  
+  inputs:
+    x: int
+    y: int
+    
+  -----------------------------------------------------------    
+  returns:
+    x-y: int
+    
+  -----------------------------------------------------------
+  """
+  
   pass
